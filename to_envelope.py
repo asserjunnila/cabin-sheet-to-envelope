dd1674f54a573153e99f4f1ad63a7b7695e9aab5
Asser Junnila <asser.junnila@gmail.com>
2021-11-21 18:41:06 +0200

    added json validation and json schema

--- to_envelope.py
+++ to_envelope.py
@@ -13,39 +13,64 @@ import os
 import sys
 import random
 import json
+import jsonschema
+from jsonschema import validate
 
 
-def parse_cabins(file_name):
-  print("INPUT \n" + file_name)
+def parse_cabins(file_name, config):
+  print(f"INPUT \n {file_name}")
   wb = openpyxl.load_workbook(file_name)
   sheet = wb.active
   cabins_arr = []
   cabin_arr = []
   current = sheet['B3'].value
-  #A=hyttiluokka
-  #B=hyttinumero
-  #C=sukunimi
-  #D=etunimi
-  #G=DIN1
-  #H=DIN2
-  #I=BRE
-  #J=LUN
-  for row in range(3, sheet.max_row + 1):
-    if sheet['A'+str(row)].value is None and sheet['B'+str(row)].value is None and sheet['C'+str(row)].value is None:
+  
+  # column A = hyttiluokka
+  cabin_class_col = config['spreadsheetconfig']['cabin_class_col']
+
+  # column B = hyttinumero
+  cabin_id_col = config['spreadsheetconfig']['cabin_id_col']
+
+  # column C = sukunimi
+  last_name_col = config['spreadsheetconfig']['last_name_col']
+
+  # column D = etunimi
+  first_name_col = config['spreadsheetconfig']['first_name_col']
+
+  # column G = DIN1
+  DIN1_col = config['spreadsheetconfig']['DIN1_col']
+
+  # column H = DIN2
+  DIN2_col = config['spreadsheetconfig']['DIN2_col']
+
+  # column I = BRE
+  BRE_col = config['spreadsheetconfig']['BRE_col']
+
+  # column J = LUN
+  LUN_col = config['spreadsheetconfig']['LUN_col']
+
+  # from what row to start the cabins
+  first_cabin_row = config['spreadsheetconfig']['first_cabin_row']
+
+
+  for row in range(first_cabin_row, sheet.max_row + 1):
+    # if row is empty, then skip to next row
+    if sheet[cabin_class_col+str(row)].value is None and sheet[cabin_id_col+str(row)].value is None and sheet[last_name_col+str(row)].value is None:
       continue
-    if sheet['B' + str(row)].value == current or sheet['B' + str(row)].value is None:
-      cabin_arr.append([sheet['A' + str(row)].value, sheet['C' + str(row)].value, sheet['D' + str(row)].value,
-      sheet['G' + str(row)].value, sheet['H' + str(row)].value, sheet['I' + str(row)].value,
-      sheet['J' + str(row)].value])
+    # if cabin id is the current or there isnt a cabin id -> the same cabin as previous row and add it to the array of this particular cabin 
+    if sheet[cabin_id_col + str(row)].value == current or sheet[cabin_id_col + str(row)].value is None:
+      cabin_arr.append([sheet[cabin_class_col + str(row)].value, sheet[last_name_col + str(row)].value, sheet[first_name_col + str(row)].value,
+      sheet[DIN1_col + str(row)].value, sheet[DIN2_col + str(row)].value, sheet[BRE_col + str(row)].value, sheet[LUN_col + str(row)].value])
+    # otherwise the next row is from another cabin -> add the existing cabin to the list of cabins
     else:
       cabins_arr.append(cabin_arr)
-      current = sheet['B' + str(row)].value
-      cabin_arr = [[sheet['A' + str(row)].value, sheet['C' + str(row)].value, sheet['D' + str(row)].value,
-      sheet['G' + str(row)].value, sheet['H' + str(row)].value, sheet['I' + str(row)].value,
-      sheet['J' + str(row)].value]]
+      current = sheet[cabin_id_col + str(row)].value
+      cabin_arr = [[sheet[cabin_class_col + str(row)].value, sheet[last_name_col + str(row)].value, sheet[first_name_col + str(row)].value,
+      sheet[DIN1_col + str(row)].value, sheet[DIN2_col + str(row)].value, sheet[BRE_col + str(row)].value,
+      sheet[LUN_col + str(row)].value]]
       if row == sheet.max_row:
-        cabin_arr.append([sheet['A' + str(row)].value, sheet['C' + str(row)].value, sheet['D' + str(row)].value,
-        sheet['G' + str(row)].value, sheet['H' + str(row)].value, sheet['I' + str(row)].value, sheet['J' + str(row)].value])
+        cabin_arr.append([sheet[cabin_class_col + str(row)].value, sheet[last_name_col + str(row)].value, sheet[first_name_col + str(row)].value,
+        sheet[DIN1_col + str(row)].value, sheet[DIN2_col + str(row)].value, sheet[BRE_col + str(row)].value, sheet[LUN_col + str(row)].value])
         cabins_arr.append(cabin_arr)
   return cabins_arr
 
@@ -80,7 +105,7 @@ def create_table(cabin_arr, styleSheet):
       t._argW[i] = 1 * cm
   return t
 
-def createPDF(CABINS_XLSX, OUTPUT_FILE):
+def createPDF(CABINS_XLSX, OUTPUT_FILE, config):
 
   quote_style = ParagraphStyle('yourtitle',
                           fontName="Helvetica-Oblique",
@@ -94,7 +119,7 @@ def createPDF(CABINS_XLSX, OUTPUT_FILE):
   # Picture name given as argument
   try:
     picture_file_name = sys.argv[1]
-  except:
+  except ValueError:
     print("pls give picture file name as an argument and try again")
     quit()
 
@@ -102,7 +127,7 @@ def createPDF(CABINS_XLSX, OUTPUT_FILE):
   I.drawHeight = 5.48 * cm
   I.drawWidth = 17.5 * cm
 
-  cabins = parse_cabins(CABINS_XLSX)
+  cabins = parse_cabins(CABINS_XLSX, config)
 
   #doc = SimpleDocTemplate(OUTPUT_FILE, pagesize=landscape(A5), topMargin=1, bottomMargin=0)
   doc = SimpleDocTemplate(OUTPUT_FILE, pagesize=landscape(A5), topMargin=1, bottomMargin=0)
@@ -111,13 +136,6 @@ def createPDF(CABINS_XLSX, OUTPUT_FILE):
 
   styleSheet = getSampleStyleSheet()
   # Add all elements to doc
-
-  try:  
-    with open('notes.json') as json_file:
-      notes = json.load(json_file)
-  except:
-    print("notes.json not found and script aborted")
-    quit()
 
   for cabin in cabins:
     # picture 
@@ -130,37 +148,57 @@ def createPDF(CABINS_XLSX, OUTPUT_FILE):
     elements.append(create_table(cabin, styleSheet))
 
     # heading 
-    elements.append(Paragraph(notes['heading'], styleSheet[
-                                "Heading4"]))
+    elements.append(Paragraph(config['heading'], styleSheet["Heading4"]))
     # first note
-    elements.append(
-        Paragraph(notes['first'],
-                styleSheet["BodyText"]))
+    elements.append(Paragraph(config['first'], styleSheet["BodyText"]))
 
     # second note
-    elements.append(Paragraph(notes['second'],
-                            styleSheet["BodyText"]))
+    elements.append(Paragraph(config['second'], styleSheet["BodyText"]))
 
     # third note
-    elements.append(
-        Paragraph(notes['third'],
-                styleSheet["BodyText"]))
+    elements.append(Paragraph(config['third'], styleSheet["BodyText"]))
 
     # more artificial padding, TODO read documentation and improve layout to avoid these ugly hacks
     elements.append(Paragraph(" ",styleSheet["BodyText"]))
     elements.append(Paragraph(" ",styleSheet["BodyText"]))
 
     # random quote 
-    elements.append(Paragraph('"'+notes['quote'][random.randrange(0,len(notes['quote']))]+'"', quote_style))
+    elements.append(Paragraph('"'+config['quote'][random.randrange(0,len(config['quote']))]+'"', quote_style))
 
     elements.append(PageBreak())
 
   # write the document to disk
   doc.build(elements)
 
+def validate_config(config):
+  # validate the json schema against the defined schema.json
+  try:  
+    with open('schema.json') as json_file:
+      schema = json.load(json_file)
+  except ValueError:
+    print("schema.json not found and script aborted")
+    quit()
+
+  try:
+    validate(instance=config, schema=schema)
+  except jsonschema.exceptions.ValidationError as err:
+    return False
+  return True
+
+
 def main():
   dirpath = os.getcwd()
   files = []
+
+  try:  
+    with open('config.json') as json_file:
+      config = json.load(json_file)
+      if validate_config(config) is False:
+        print("config file invalid, quitting")
+        quit()
+  except ValueError:
+    print("config.json not found and script aborted")
+    quit()
 
   # r=root, d=directories, f = files
   for r, d, f in os.walk(dirpath):
@@ -178,7 +216,8 @@ def main():
       pass
       # print("envelope_print folder already exists so let's use that one")
 
-    createPDF(xlsx, path + "/" + folder_name + "/" + "envelope_print_" + xlsx.split('/')[-1].split('.')[0]+".pdf")
-    print("OUTPUT: \n" + "/" + folder_name + "/envelope_print_" + xlsx.split('/')[-1].split('.')[0]+".pdf")
+    createPDF(xlsx, f"{path}/{folder_name}/{folder_name}_{xlsx.split('/')[-1].split('.')[0]}.pdf", config)
+
+    print(f"OUTPUT: \n /{folder_name}/{folder_name}_{xlsx.split('/')[-1].split('.')[0]}.pdf")
 
 main()
